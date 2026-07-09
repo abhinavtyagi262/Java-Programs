f734489b64ae0a15b237103a025f4526889dd376
Abhinav Tyagi <abhinavtyagi262@gmail.com>
2026-07-10 04:43:10 +0530

    Renamed Files

--- src/codingquestions/StringVowelElimination.java
+++ src/codingquestions/StringVowelElimination.java
@@ -2,7 +2,7 @@ package codingquestions;
 
 import java.util.Scanner;
 
-public class StringVowelEliminationClass {
+public class StringVowelElimination {
 
 	public static void main(String[] args) {
 		System.out.println("Enter a string: ");
