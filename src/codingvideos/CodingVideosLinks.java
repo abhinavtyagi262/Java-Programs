892a5bad3aeda751dfaad02e1126041b5379fee3
Abhinav Tyagi <abhinavtyagi262@gmail.com>
2026-07-10 12:01:11 +0530

    Added CodingVideosLinks

---
+++ src/codingvideos/CodingVideosLinks.java
@@ -0,0 +1,25 @@
+package codingvideos;
+
+public class CodingVideosLinks {
+
+	public static void main(String[] args) {
+		
+// Internal functionality of HashMap: https://youtu.be/CojCE-ojdGY
+
+// Joins in SQL: https://youtu.be/H6988OpZKTU	
+// Group By and Having Clause in SQL: https://youtu.be/SvJLXj05cow?si=C5A1UHNcQ-CNL-aq
+// Aggregate Functions in SQL: https://youtu.be/9NfthspfXEo?si=hPdPFfwfpjEaYyof
+
+// Circuit Breaker Design Pattern in Spring Boot 1: https://youtube.com/shorts/bKZ6kJcYSgo?si=EBxpAuCVKY-XVtFr
+// Circuit Breaker Design Pattern in Spring Boot 2: https://youtube.com/shorts/oKp2xApq_iw?si=MENiPLIMuBLyMvi7
+
+/* Spring Dependency Injection: https://youtube.com/shorts/MLDf-4kkpHA?si=5j9WD4e9yY-0gyYe
+   Field Injection Vs Constructor Injection: https://youtube.com/shorts/cS9bZgmG_04?si=v3VPrpa2uVABuJlI
+   Spring Dependency Injection (Injecting Dependencies Dynamically)
+   1) Field Injection
+   2) Setter Injection
+   3) Constructor Injection
+*/
+
+	}
+}
